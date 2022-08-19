4811c1ec8fc880010ac75e73d1d1833ba2c94037
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-08-20 03:14:31 +0500

    bank contract added

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -9,4 +9,10 @@ contract Bank {
     function deposit() external payable {
         balanceOf[msg.sender] += msg.value;
     }
+
+    function withdraw() external {
+        uint256 depositedAmount = balanceOf[msg.sender];
+        payable(msg.sender).sendValue(depositedAmount);
+        balanceOf[msg.sender] = 0;
+    }
 }
--- test/Reentrancy.js
+++ test/Reentrancy.js
@@ -6,10 +6,12 @@ describe("Reentrancy", () => {
   let bank;
 
   beforeEach(async () => {
-    [deployer] = await ethers.getSigners();
+    [deployer, user] = await ethers.getSigners();
     const Bank = await ethers.getContractFactory("Bank", deployer);
     bank = await Bank.deploy();
+
     await bank.deposit({ value: ethers.utils.parseEther("1") });
+    await bank.connect(user).deposit({ value: ethers.utils.parseEther("69") });
   });
 
   describe("Facilitates the deposits and withdraws", () => {
@@ -17,6 +19,20 @@ describe("Reentrancy", () => {
       //Check deposit balance
       const deployerBalance = await bank.balanceOf(deployer.address);
       expect(deployerBalance.toString()).to.equal(ethers.utils.parseEther("1"));
+
+      const userBalance = await bank.balanceOf(user.address);
+      expect(userBalance.toString()).to.equal(ethers.utils.parseEther("69"));
+    });
+
+    
+    it("accepts the withdraws", async () => {
+      await bank.withdraw();
+
+      const deployerBalance = await bank.balanceOf(deployer.address);
+      const userBalance = await bank.balanceOf(user.address);
+
+      expect(userBalance.toString()).to.equal(ethers.utils.parseEther("69"));
+      expect(deployerBalance.toString()).to.equal(ethers.utils.parseEther("0"));
     });
   });
 });
